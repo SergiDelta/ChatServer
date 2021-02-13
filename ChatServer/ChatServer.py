93899cc207576f9ae54b23e8479f873a462c3913
SergiDelta <sergiogl11042@gmail.com>
2021-02-13 22:59:25 +0100

    Add files via upload

---
+++ ChatServer/ChatServer.py
@@ -0,0 +1,116 @@
+#													 
+# Author: Sergio Garcia Lopez						 
+# GitHub: https://github.com/SergiDelta/ChatServer	 
+# Date: February 2021								 
+# Description: Simple chat server that uses threads 
+#   to handle TCP connections. It includes chat      
+#   records, timeout handling and broadcasting.      
+#													 
+
+import socket
+import sys
+import threading
+import datetime
+
+timeout = 60
+
+
+class ChatServer:
+
+   def __init__(self, addr, file):
+
+      self.host = addr[0]
+      self.port = addr[1]
+      self.socklist = []
+      self.record = file
+      self.record.write("Session date: " + datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S") +"\n\n" )
+      self.serversock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+      self.serversock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+      self.socklist.append(self.serversock)
+      print("Socket created")
+
+      try:
+         self.serversock.bind( (self.host,self.port) )
+      except socket.error as e:
+         print(e)
+         sys.exit()
+
+      print("Socket binded")
+
+      self.serversock.listen(10)
+      print("Server listening on port " + str(self.port) + "\n")
+
+   def clientthread(self, conn):
+
+      addr = conn.getpeername()
+      self.socklist.append(conn)
+      conn.settimeout(timeout)
+      conn.sendall("Welcome to the server. Type something and hit enter\n".encode() )
+
+      while True:
+
+         try:
+
+            data = conn.recv(1024)
+
+         except socket.timeout as t:
+
+            print("Timeout. ", end='' )
+            self.record.write("Timeout. ")
+            break
+
+         if not data:
+            break
+
+         self.broadcast(data.decode(), conn)
+
+      self.socklist.remove(conn)
+      conn.sendall("Timeout. Connection lost with server\n".encode() )
+      conn.close()
+      print("Connection closed with [" + addr[0] + ":" + str(addr[1]) + "]\n" )
+      self.record.write("Connection closed with [" + addr[0] + ":" + str(addr[1]) + "]\n")
+
+   def broadcast(self, msg, sender):
+
+      addr = sender.getpeername()
+      fullmsg = "[" + addr[0] + ":" + str(addr[1]) + "] " + msg
+
+      for sock in self.socklist:
+         if sock != self.serversock and sock != sender:
+            sock.sendall(fullmsg.encode() )
+
+      print(fullmsg)
+      self.record.write(fullmsg)
+
+   def run(self):
+
+      while True:
+
+         conn, addr = self.serversock.accept()
+         print("Connected with [" + addr[0] + ":" + str(addr[1]) + "]\n" )
+         self.record.write("Connected with [" + addr[0] + ":" + str(addr[1]) + "]\n")
+         threading.Thread(target=self.clientthread, args=(conn,) ).start()
+
+
+def main():
+
+   if len(sys.argv) != 3:
+      print("Use: " + sys.argv[0] + " <ip> " + "<port>")
+      sys.exit()
+
+   host = sys.argv[1]
+   port = int(sys.argv[2])
+
+   file = open("record.txt", "a")
+
+   try:
+      myServer = ChatServer( (host, port) , file)
+      myServer.run()
+   except KeyboardInterrupt:
+      print()
+
+   file.write("\n")
+   file.close()
+
+if __name__ == "__main__":
+   main()
---
+++ ChatServer/client.py
@@ -0,0 +1,61 @@
+#                                                        
+# Author: Sergio Garcia Lopez                            
+# GitHub: https://github.com/SergiDelta/ChatServer       
+# Date: February 2021                                    
+# Description: Client programm example for ChatServer.py 
+#                                                        
+
+import socket
+import sys
+import select
+
+if len(sys.argv) != 3:
+   print("Use: " + sys.argv[0] + " <host> " + "<port>")
+   sys.exit()
+
+host = sys.argv[1]
+port = int(sys.argv[2])
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+try:
+   print("Trying to connect...")
+   s.connect((host,port))
+
+except socket.error as e:
+   print(e)
+   sys.exit()
+
+print("Connected")
+
+try:
+
+   while True:
+
+      try:
+
+         rlist, wlist, xlist = select.select([s], [], [], 0.5)
+
+         if [rlist, wlist, xlist] != [ [], [], [] ]:
+
+            rx_msg = s.recv(1024)
+            rx_decoded = rx_msg.decode()
+
+            if rx_decoded[ len(rx_decoded) - 1 ] == '\n':
+               rx_list = list(rx_decoded)
+               rx_list[ len(rx_decoded) - 1 ] = ''
+               rx_decoded = ''.join(rx_list)
+
+            print(rx_decoded)
+
+            if rx_decoded.find("Timeout") != -1:
+               break
+
+         tx_msg = input(">> ")
+         s.sendall( (tx_msg + "\n").encode() )
+
+      except socket.error:
+         break
+
+except KeyboardInterrupt:
+   print()
